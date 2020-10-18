336a3077b5c27ea55bc2223aa7bd4f779bf268dd
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2020-10-18 07:46:17 -0400

    Create create_user3.sh

---
+++ create_user3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Purpose - Script to add a user to Linux system including passsword
+# Author - Vivek Gite <www.cyberciti.biz> under GPL v2.0+
+# ------------------------------------------------------------------
+# Am i Root user?
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+fi
